d479195d84063039e107dbac45b7fa50a5825cec
kellimohr <kellimohr@gmail.com>
2013-10-21 20:57:11 -0700

    Cookie Demo.

---
+++ HomeworkTwo/KMCookieDetailViewController.h
@@ -0,0 +1,15 @@
+//
+//  KMCookieDetailViewController.h
+//  HomeworkTwo
+//
+//  Created by Kelli Mohr on 10/21/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMCookieDetailViewController : UITableViewController
+
+@property (nonatomic) NSMutableArray *cookies;
+
+@end
---
+++ KMCookie.m
@@ -0,0 +1,13 @@
+//
+//  KMCookie.m
+//  HomeworkTwo
+//
+//  Created by Kelli Mohr on 10/21/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import "KMCookie.h"
+
+@implementation KMCookie
+
+@end
