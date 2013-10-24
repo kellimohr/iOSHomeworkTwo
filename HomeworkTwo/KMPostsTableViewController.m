4c597bdfed990fb36fe93ad6d7dd89e9f91ab2bb
kellimohr <kellimohr@gmail.com>
2013-10-24 16:47:27 -0700

    Changed controller to use UITableView instead of the custom one I made.  Concatenated the values from the array to display on the screen.

--- HomeworkTwo/KMPostsTableViewController.m
+++ HomeworkTwo/KMPostsTableViewController.m
@@ -41,15 +41,63 @@
     post2.content = @"I really really hope I'm doing this right.";
     post2.timeStamp = [NSDate date];
     
-    _posts = [NSArray arrayWithObjects: post1, post2, nil];
+    KMPost *post3 = [[KMPost alloc] init];
+    post3.userName = @"Kelli Mohr";
+    post3.title = @"More iOS Tables";
+    post3.content = @"I went down a crazy path.";
+    post3.timeStamp = [NSDate date];
     
-    NSLog(@"%d", [_posts count]);
+    KMPost *post4 = [[KMPost alloc] init];
+    post4.userName = @"Kelli Mohr";
+    post4.title = @"More iOS Tables";
+    post4.content = @"Is there any way back?!";
+    post4.timeStamp = [NSDate date];
+    
+    KMPost *post5 = [[KMPost alloc] init];
+    post5.userName = @"Kelli Mohr";
+    post5.title = @"More iOS Tables";
+    post5.content = @"Ugh. Format Date to String";
+    post5.timeStamp = [NSDate date];
+    
+    KMPost *post6 = [[KMPost alloc] init];
+    post6.userName = @"Kelli Mohr";
+    post6.title = @"More iOS Tables";
+    post6.content = @"I really really hope I'm doing this right.";
+    post6.timeStamp = [NSDate date];
+    
+    KMPost *post7 = [[KMPost alloc] init];
+    post7.userName = @"Kelli Mohr";
+    post7.title = @"More iOS Tables";
+    post7.content = @"I really really hope I'm doing this right.";
+    post7.timeStamp = [NSDate date];
+    
+    KMPost *post8 = [[KMPost alloc] init];
+    post8.userName = @"Kelli Mohr";
+    post8.title = @"More iOS Tables";
+    post8.content = @"I really really hope I'm doing this right.";
+    post8.timeStamp = [NSDate date];
+    
+    KMPost *post9 = [[KMPost alloc] init];
+    post9.userName = @"Kelli Mohr";
+    post9.title = @"More iOS Tables";
+    post9.content = @"I really really hope I'm doing this right.";
+    post9.timeStamp = [NSDate date];
+    
+    KMPost *post10 = [[KMPost alloc] init];
+    post10.userName = @"Kelli Mohr";
+    post10.title = @"More iOS Tables";
+    post10.content = @"I really really hope I'm doing this right.";
+    post10.timeStamp = [NSDate date];
+    
+    _posts = [NSArray arrayWithObjects: post1, post2, post3, post4, post5, post6, post7,
+                                        post8, post9, post10, nil];
+    
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+       self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning
@@ -77,14 +125,22 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *CellIdentifier = @"PostTableViewCell";
-    KMPostTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
     //cell.post = [_posts[indexPath.row]];
-    cell.userNameText.text = [_posts[indexPath.row] userName];
-    cell.titleText.text = [_posts[indexPath.row] title];
-    cell.contentView.text = [_posts[indexPath.row] content];
-    cell.dateText.text = [_posts[indexPath.row] date];
+    //cell.userNameText.text = [_posts[indexPath.row] userName];
+    //cell.titleText.text = [_posts[indexPath.row] title];
+    //cell.contentView.text = [_posts[indexPath.row] content];
+    //cell.dateText.text = [_posts[indexPath.row] timeStamp];
+    
+    NSDate *postDate = [_posts[indexPath.row] timeStamp];
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateStyle:NSDateFormatterShortStyle];
+    NSString *dateString = [dateFormat stringFromDate:postDate];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ - %@",[_posts[indexPath.row] title], [_posts[indexPath.row] userName] ];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%@  %@", [_posts[indexPath.row] content], dateString];
     
     return cell;
 }
@@ -98,7 +154,7 @@
 }
 */
 
-/*
+
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -110,7 +166,7 @@
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
+
 
 /*
 // Override to support rearranging the table view.
