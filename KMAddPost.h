38a1e422b45913f1784b8501fe5a3d433028c514
kellimohr <kellimohr@gmail.com>
2013-10-27 14:16:38 -0700

    Added a Navigation controller. Added a new view Controller for editing.

---
+++ KMAddPost.h
@@ -0,0 +1,13 @@
+//
+//  KMAddPost.h
+//  HomeworkTwo
+//
+//  Created by Kelli Mohr on 10/24/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMAddPost : UIViewController
+
+@end
---
+++ KMAddPost.m
@@ -0,0 +1,38 @@
+//
+//  KMAddPost.m
+//  HomeworkTwo
+//
+//  Created by Kelli Mohr on 10/24/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import "KMAddPost.h"
+
+@interface KMAddPost ()
+
+@end
+
+@implementation KMAddPost
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
