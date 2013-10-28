6bd097579889f1829b0a1dc6640cb3e4ac5941ec
kellimohr <kellimohr@gmail.com>
2013-10-28 13:46:14 -0700

    Created a UIColor category/

---
+++ HomeworkTwo/KMColorOptions.h
@@ -0,0 +1,17 @@
+//
+//  KMColorOptions.h
+//  HomeworkTwo
+//
+//  Created by Kelli Mohr on 10/28/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (ColorOptions)
+
++ (UIColor *) randomColor;
+
++ (UIColor *) lightenColor;
+
+@end
---
+++ HomeworkTwo/KMColorOptions.m
@@ -0,0 +1,26 @@
+//
+//  KMColorOptions.m
+//  HomeworkTwo
+//
+//  Created by Kelli Mohr on 10/28/13.
+//  Copyright (c) 2013 Kelli Mohr. All rights reserved.
+//
+
+#import "KMColorOptions.h"
+
+@implementation UIColor (ColorOptions)
+
++ (UIColor *) randomColor
+{
+    UIColor *newColor = nil;
+    return newColor;
+    
+}
+
++ (UIColor *) lightenColor
+{
+    UIColor *lighter = nil;
+    return lighter;
+}
+
+@end
